2339d9ad0e0829785fb28e85a7ba2ad605096f45
MaratMM <woodi94@mail.ru>
2023-02-02 12:19:10 +0300

    продолжаем 49 урок

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,7 +1,32 @@
 export let Users = (props) => {
+    
+    let UsersData = props.users.map((elem)=>{
+        return (
+            <div>
+                <div>
+                    <img src="#" alt="profilePhoto" />
+                    <button>unFollow</button>
+                </div>
+                <div>
+                    <div>
+                        <span>{elem.fullName}</span>
+                        <span>{elem.statusProfile}</span>
+                    </div>
+                    <div>
+                        <span>{elem.location.city}</span>
+                        <span></span>
+                        
+                        {elem.location.country}
+                    </div>
+                </div>
+            </div>
+        )
+    })
+
     return (
         <div>
             <h1>Hellow users</h1>
+            {UsersData}
         </div>
     )
 }
